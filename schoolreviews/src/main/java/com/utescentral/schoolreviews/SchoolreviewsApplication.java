d8eb4cdc1aaa8f60d234210a18bedae8a2e2e24a
saidmmendez <enzo.mendez315@gmail.com>
2024-12-23 02:52:24 -0700

    Springboot setup.

---
+++ schoolreviews/src/main/java/com/utescentral/schoolreviews/SchoolreviewsApplication.java
@@ -0,0 +1,13 @@
+package com.utescentral.schoolreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolreviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolreviewsApplication.class, args);
+	}
+
+}
---
+++ schoolreviews/src/test/java/com/utescentral/schoolreviews/SchoolreviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utescentral.schoolreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolreviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
